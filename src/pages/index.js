7464e02181bf0e256cc139a191163e409c914753
Lucas Zapico <zapicolucas@gmail.com>
2021-08-25 10:28:31 -0600

    syntax and ssr  colormode bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,28 @@
-import {
-  useColorMode,
-  Heading,
-  Box,
-  Container,
-  GridItem,
-  Grid,
-  Flex,
-} from '@chakra-ui/react';
-import * as React from 'react';
-import { graphql } from 'gatsby';
-import { CardOne } from '../components/_index';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, {
-  Navigation, Pagination, Scrollbar, A11y,
-} from 'swiper';
-import { generate } from 'shortid';
-import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { Heading, Box, Container, GridItem, Grid, Flex } from '@chakra-ui/react'
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import { CardOne } from '../components/_index'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { generate } from 'shortid'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import {
   BlurBlobOne,
   BlurBlobTwo,
   BlurBlobThree,
-} from '../components/flare/blur-blobs';
+} from '../components/flare/blur-blobs'
 
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 const HomeHero = () => {
-  const { colorMode } = useColorMode();
+  const colorMode = 'dark'
 
-  const color = colorMode === 'light' ? '#38383a' : '#f9f6f2';
+  const color = colorMode === 'light' ? '#38383a' : '#f9f6f2'
 
   const content = {
     header: 'Hello, I am Lucas. Thanks for stopping by.',
     subHeader: 'Mostly a Frontend developer focusing on Reactjs.',
-  };
+  }
   return (
     <Box py={20}>
       <Container maxW="container.xl">
@@ -56,87 +46,57 @@ const HomeHero = () => {
           </Heading>
         )}
 
-        <Heading
-          as="h1"
-          size="2xl"
-          variant="sec"
-        >
+        <Heading as="h1" size="2xl" variant="sec">
           {content.subHeader}
         </Heading>
       </Container>
     </Box>
-  );
-};
+  )
+}
 
 const ProjectCard = ({ project, i }) => {
-  const { title, subHeader, featuredImage } = project.node.frontmatter;
-  const image = getImage(featuredImage);
+  const { title, subHeader, featuredImage } = project.node.frontmatter
+  const image = getImage(featuredImage)
   return (
     <GridItem colSpan={{ base: 6, md: `${i % 3 === 0 ? 6 : 3}` }}>
-      <CardOne
-        display="flex"
-        flexWrap="wrap"
-      >
+      <CardOne display="flex" flexWrap="wrap">
         <Box width={{ base: '100%', lg: '40%' }}>
-          <Heading
-            as="h4"
-            size="lg"
-          >
+          <Heading as="h4" size="lg">
             {title}
           </Heading>
-          <Heading
-            as="h5"
-            variant="sec"
-            size="md"
-          >
+          <Heading as="h5" variant="sec" size="md">
             {subHeader}
           </Heading>
         </Box>
         <Box width={{ base: '100%', lg: '60%' }}>
-          <GatsbyImage
-            image={image}
-            alt={`${title} featured Image`}
-          />
+          <GatsbyImage image={image} alt={`${title} featured Image`} />
         </Box>
       </CardOne>
     </GridItem>
-  );
-};
+  )
+}
 
 const ProjectSections = ({ recentProjects }) => {
   return (
     <Box py={20}>
       <Container maxW="container.xl">
-        <Heading
-          as="h3"
-          size="3xl"
-        >
+        <Heading as="h3" size="3xl">
           Recent Projects
         </Heading>
-        <Grid
-          templateColumns="repeat(6, 1fr)"
-          gap={10}
-        >
+        <Grid templateColumns="repeat(6, 1fr)" gap={10}>
           {recentProjects.edges.map((project, i) => (
-            <ProjectCard
-              i={i}
-              key={generate()}
-              project={project}
-            />
+            <ProjectCard i={i} key={generate()} project={project} />
           ))}
         </Grid>
       </Container>
     </Box>
-  );
-};
+  )
+}
 
 const ArticleSections = ({ recentArticles }) => (
   <Box py={20}>
     <Container maxW="container.xl">
-      <Heading
-        as="h3"
-        size="3xl"
-      >
+      <Heading as="h3" size="3xl">
         Recent Articles
       </Heading>
     </Container>
@@ -158,23 +118,11 @@ const ArticleSections = ({ recentArticles }) => (
       >
         {recentArticles.edges.map((article, i) => (
           <SwiperSlide key={generate()}>
-            <CardOne
-              mt={6}
-              minH="20rem"
-              minW="20rem"
-            >
-              <Heading
-                as="h4"
-                size="xl"
-                variant="sec"
-              >
+            <CardOne mt={6} minH="20rem" minW="20rem">
+              <Heading as="h4" size="xl" variant="sec">
                 {article.node.frontmatter.title}
               </Heading>
-              <Heading
-                as="h5"
-                variant="sec"
-                size="md"
-              >
+              <Heading as="h5" variant="sec" size="md">
                 {article.node.frontmatter.description}
               </Heading>
             </CardOne>
@@ -183,7 +131,7 @@ const ArticleSections = ({ recentArticles }) => (
       </Swiper>
     </Box>
   </Box>
-);
+)
 
 const Flare = () => (
   <Box
@@ -196,56 +144,31 @@ const Flare = () => (
     overflow="hidden"
     // bgGradient="linear(to-br, brand.zero, brand.two)"
   >
-    <Box
-      position="relative"
-      height="100%"
-      width="100%"
-      opacity="0.7"
-    >
-      <Box
-        pos="absolute"
-        className="home-blob-one"
-        right="0px"
-        top="0px"
-      >
+    <Box position="relative" height="100%" width="100%" opacity="0.7">
+      <Box pos="absolute" className="home-blob-one" right="0px" top="0px">
         <BlurBlobOne />
-      </Box>
-      {' '}
-      <Box
-        pos="absolute"
-        left="0px"
-        bottom="0px"
-        className="home-blob-two"
-      >
+      </Box>{' '}
+      <Box pos="absolute" left="0px" bottom="0px" className="home-blob-two">
         <BlurBlobTwo />
-      </Box>
-      {' '}
-      <Box
-        pos="absolute"
-        left="0px"
-        top="0px"
-        className="home-blob-two"
-      >
+      </Box>{' '}
+      <Box pos="absolute" left="0px" top="0px" className="home-blob-two">
         <BlurBlobThree />
       </Box>
     </Box>
   </Box>
-);
+)
 
 const IndexPage = ({ data }) => {
-  const { recentArticles, recentProjects } = data;
+  const { recentArticles, recentProjects } = data
   return (
-    <Box
-      position="relative"
-      zIndex="0"
-    >
+    <Box position="relative" zIndex="0">
       <HomeHero />
       <ProjectSections recentProjects={recentProjects} />
       <ArticleSections recentArticles={recentArticles} />
       <Flare />
     </Box>
-  );
-};
+  )
+}
 
 export const query = graphql`
   query {
@@ -290,6 +213,6 @@ export const query = graphql`
       }
     }
   }
-`;
+`
 
-export default IndexPage;
+export default IndexPage
