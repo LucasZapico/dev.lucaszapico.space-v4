522b102b6d67b923071984e754ee6f33516967e0
Lucas Zapico <zapicolucas@gmail.com>
2021-08-25 07:52:45 -0600

    add: reducer

---
+++ src/components/_index.js
@@ -0,0 +1,11 @@
+// base
+export { LinkOne, LinkPhone, LinkEmail } from './base/buttons-links';
+export { default as Header } from './base/header';
+export { default as Footer } from './base/footer';
+export { default as Nav } from './base/nav';
+
+// elements
+export { CardOne } from './elements/card';
+
+// sections
+export { default as SiteCredits } from './sections/site-credit';
