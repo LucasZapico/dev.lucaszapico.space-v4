405550b65c7a963e6c537f329d6c89b76bbf5436
Lucas Zapico <zapicolucas@gmail.com>
2021-08-25 07:53:00 -0600

    add: home page

--- src/pages/site-credits.js
+++ src/pages/site-credits.js
@@ -1,22 +1,29 @@
-import { Box, Container, Heading } from '@chakra-ui/react'
-import React from 'react'
-import { SiteCredits } from '../components/_index'
+import { Box, Container, Heading } from '@chakra-ui/react';
+import React from 'react';
+import { SiteCredits } from '../components/_index';
 
 const SiteCreditPage = () => {
   return (
     <Box minH="100vh">
       <Container maxW="container.md">
-        <Heading as="h1" size="3xl">
+        <Heading
+          as="h1"
+          size="3xl"
+        >
           Site Credits
         </Heading>
-        <Heading as="h2" size="" variant="sec">
+        <Heading
+          as="h2"
+          size=""
+          variant="sec"
+        >
           Shout out to all of the amazing folks, projects, etc that enable me to
           build
         </Heading>
         <SiteCredits />
       </Container>
     </Box>
-  )
-}
+  );
+};
 
-export default SiteCreditPage
+export default SiteCreditPage;
