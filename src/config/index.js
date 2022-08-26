7ab1a0d5e3c951b28c94f46c1657531be99449c6
Lucas zapico <12178702+LucasZapico@users.noreply.github.com>
2022-08-26 10:09:56 -0600

    feat: :sparkles: enhance article section

---
+++ src/config/index.js
@@ -0,0 +1,27 @@
+const ROUTES = [
+  {
+    name: 'Home',
+    path: '/',
+  },
+  {
+    name: 'Projects',
+    path: '/projects',
+  },
+  // {
+  //   name: 'Articles',
+  //   path: '/articles',
+  // },
+  {
+    name: 'Notes',
+    path: '/notes',
+  },
+  {
+    name: 'About',
+    path: '/about',
+  },
+
+  // {
+  //   name: 'Playground',
+  //   path: '/playground',
+  // },
+]
