ab0592572f7b47730f3a9c6684764e479d19283a
Lucas zapico <12178702+LucasZapico@users.noreply.github.com>
2022-08-26 10:08:14 -0600

    feat: :sparkles: enhance project case ui

--- src/components/pages/projects/index.js
+++ src/components/pages/projects/index.js
@@ -3,6 +3,7 @@ import { graphql, useStaticQuery, Link as GatsbyLink } from 'gatsby'
 import { SearchIcon } from '@chakra-ui/icons'
 
 import {
+  Flex,
   Box,
   Heading,
   Container,
@@ -13,10 +14,65 @@ import {
 import { generate } from 'shortid'
 import { generateImageData } from 'gatsby-plugin-image'
 import ProjectCard from './project-card'
-import { ProjectSection } from '../../_index'
+import { ProjectSection, CardOne, Tag } from '../../_index'
+
+const TagCloud = ({ recentProjects, filter, setFilter }) => {
+  const [cats, setCats] = useState({})
+
+  function handleCats() {
+    const tmpCat = {}
+    recentProjects.edges.forEach((e, i) => {
+      e.node.frontmatter.categories.forEach((c, j) => {
+        if (!(c in cats)) {
+          tmpCat[c] = 1
+        } else {
+          tmpCat[c] += 1
+        }
+      })
+    })
+    setCats(tmpCat)
+  }
+
+  useEffect(() => {
+    handleCats()
+  }, [])
+
+  return (
+    <Flex flexWrap="wrap">
+      {Object.keys(cats).map((c) => {
+        return (
+          <Tag mb={2} hasBG={filter === c} onClick={() => setFilter(c)}>
+            {c}
+          </Tag>
+        )
+      })}
+    </Flex>
+  )
+}
 
 const Projects = () => {
-  // const { recentProjects } = useStaticQuery(query)
+  const { recentProjects } = useStaticQuery(query)
+  const [filter, setFilter] = useState('')
+  const [projects, setProjects] = useState(recentProjects)
+
+  function handleFilter(currentFilter) {
+    return recentProjects.edges.filter((e, i) => {
+      const cats = e.node.frontmatter.categories
+      if (cats.includes(currentFilter)) {
+        return e
+      }
+    })
+  }
+
+  useEffect(() => {
+    const filteredProj = handleFilter(filter)
+
+    if (filter !== '') {
+      setProjects((prev) => {
+        return { edges: filteredProj }
+      })
+    }
+  }, [filter])
 
   return (
     <>
@@ -24,43 +80,49 @@ const Projects = () => {
         <Heading as="h1" size="4xl">
           Work
         </Heading>
-
-        {/* <ProjectSection recentProjects={recentProjects} /> */}
+        {projects && (
+          <TagCloud
+            recentProjects={projects}
+            setFilter={setFilter}
+            filter={filter}
+          />
+        )}
+        {projects && <ProjectSection recentProjects={projects} />}
       </Container>
     </>
   )
 }
 
-// export const query = graphql`
-//   query {
-//     recentProjects: allMarkdownRemark(
-//       filter: {
-//         frontmatter: { isdraft: { eq: false }, type: { eq: "project" } }
-//       }
-//       sort: { fields: frontmatter___date_created, order: DESC }
-//     ) {
-//       edges {
-//         node {
-//           id
-//           fields {
-//             path
-//           }
-//           frontmatter {
-//             title
-//             subHeader
-//             categories
-//             description
-//             tags
-//             featuredImage {
-//               childImageSharp {
-//                 gatsbyImageData
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+export const query = graphql`
+  query {
+    recentProjects: allMarkdownRemark(
+      filter: {
+        frontmatter: { isdraft: { eq: false }, type: { eq: "project" } }
+      }
+      sort: { fields: frontmatter___date_created, order: DESC }
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            path
+          }
+          frontmatter {
+            title
+            subHeader
+            categories
+            description
+            tags
+            featuredImage {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Projects
