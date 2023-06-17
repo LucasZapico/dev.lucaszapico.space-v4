086fcee3d9b42ce845a113a7b28c0c00d2b2d46b
Lucas Zapico <dev@lucaszapico.space>
2023-06-16 22:31:28 -0700

    feat(ui) enhance styling

--- src/components/modules/mdx-toc.js
+++ src/components/modules/mdx-toc.js
@@ -12,12 +12,28 @@ const TOCChildren = ({ pagePath, items, iterator }) => {
       {items.map((t) => {
         // console.log(typeof t.title)
         // console.log(t)
-        if(typeof t.title === "string"){
+        if (typeof t.title === "string") {
           const name = `${t.title.slice(0, 1).toUpperCase()}${t.title.slice(1)}`
 
-        if (Object.prototype.hasOwnProperty.call(t, "items")) {
-          return (
-            <Flex key={generate()} flexDir="column">
+          if (Object.prototype.hasOwnProperty.call(t, "items")) {
+            return (
+              <Flex key={generate()} flexDir="column">
+                <Link
+                  variant="aside"
+                  ml={iterator * 4}
+                  key={generate()}
+                  as={GatsbyLink}
+                  to={`${t.url}`}
+                  mb={2}
+                  fontSize="xs"
+                >
+                  <span>&#8226;</span> {name}
+                </Link>
+                <TOCChildren items={t.items} iterator={iterator + 1} />
+              </Flex>
+            )
+          } else {
+            return (
               <Link
                 variant="aside"
                 ml={iterator * 4}
@@ -29,28 +45,11 @@ const TOCChildren = ({ pagePath, items, iterator }) => {
               >
                 <span>&#8226;</span> {name}
               </Link>
-              <TOCChildren items={t.items} iterator={iterator + 1} />
-            </Flex>
-          )
-        } else {
-          return (
-            <Link
-              variant="aside"
-              ml={iterator * 4}
-              key={generate()}
-              as={GatsbyLink}
-              to={`${t.url}`}
-              mb={2}
-              fontSize="sm"
-            >
-              <span>&#8226;</span> {name}
-            </Link>
-          )
+            )
+          }
         }
-      }
-      })
-    }
-        
+        return null
+      })}
     </>
   )
 }
@@ -64,13 +63,17 @@ const MdxTOC = ({ tableOfContents, pagePath }) => {
       py={6}
       px={4}
       as="aside"
-      
-      // overflowY="scroll"
     >
       <Heading as="div" variant="tri" mb={2} size="md">
         Table of Contents
       </Heading>
-      <Box display="flex" flexDir="column" as="nav" >
+      <Box
+        aria-label="table of contents"
+        display="flex"
+        flexDir="column"
+        as="nav"
+        
+      >
         <TOCChildren
           items={tableOfContents.items}
           pagePath={pagePath}
