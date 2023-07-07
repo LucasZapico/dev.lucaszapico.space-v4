67ef5347731032e6ba4f9d2ecc168fee2c122f71
Lucas Zapico <dev@lucaszapico.space>
2023-07-07 10:43:48 -0700

    feat(style) add glass style to components

--- src/components/elements/cards/card.js
+++ src/components/elements/cards/card.js
@@ -6,20 +6,21 @@ export const CardOne = ({ content, children, ...rest }) => {
   const { colorMode } = useColorMode()
   return (
     <Box
+      className={colorMode === "dark" ? "glass dark" : "glass light"}
       p={6}
       borderRadius="md"
       backgroundColor={colorMode === "dark" ? "gray.900" : "gray.50"}
       // boxShadow={`10px -10px ${colorMode === "light" ? "#38383a" : "#dbe3df"}`}
       _hover={{
-        bg: colorMode === "dark" ? "gray.800" : "gray.100"
+        bg: colorMode === "dark" ? "gray.800" : "gray.100",
       }}
       _active={{
-        bg: colorMode === "dark" ? "gray.700" : "gray.200"
+        bg: colorMode === "dark" ? "gray.700" : "gray.200",
       }}
       _focus={{
-        bg: colorMode === "dark" ? "gray.600" : "gray.300"
+        bg: colorMode === "dark" ? "gray.600" : "gray.300",
       }}
-      transition='all 0.2s ease-in-out'
+      transition="all 0.2s ease-in-out"
       {...rest}
     >
       {children || content}
