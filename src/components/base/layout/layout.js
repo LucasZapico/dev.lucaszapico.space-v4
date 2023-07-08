360dff248daab0aa4c55a51ced61f616982d8d97
Lucas Zapico <dev@lucaszapico.space>
2023-07-08 08:43:22 -0700

    perf moved blocking google font to chakra

--- src/components/base/layout/layout.js
+++ src/components/base/layout/layout.js
@@ -12,18 +12,6 @@ const TypeImports = () => (
   <Helmet>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@400;500;600;700&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Rampart+One&display=swap"
-      rel="stylesheet"
-    />
   </Helmet>
 )
 
